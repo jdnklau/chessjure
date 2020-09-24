d745d3a53739814035265d3637b3aff55c32e57f
Jannik Dunkelau <jdnklau@gmail.com>
2020-09-25 00:10:05 +0200

    Add line-of-sight

--- src/chessjure/moves.clj
+++ src/chessjure/moves.clj
@@ -44,8 +44,76 @@
 (defn add-to-pos
   "Shifts the values of the `pos` by `addc` in the column and `addr` in the
    row. `addc` and `addr` are integers."
- [[col row :as pos] [addc addr]]
+  [[col row :as pos] [addc addr]]
   (let [ridx (row-index row)
-        cidx (col-index col)]
-    [(nth col-keys (+ cidx addc) nil)
-     (nth row-keys (+ ridx addr) nil)]))
+        cidx (col-index col)
+        new-col (nth col-keys (+ cidx addc) nil)
+        new-row (nth row-keys (+ ridx addr) nil)]
+    (cond
+      (nil? new-col) nil
+      (nil? new-row) nil
+      :else [new-col new-row])))
+
+(defn exceeding-edge? [[col row] [dx dy]]
+  (cond
+    (and (= col :a) (< dx 0)) true
+    (and (= col :h) (> dx 0)) true
+    (and (= row :1) (< dy 0)) true
+    (and (= row :8) (> dy 0)) true
+    :else false))
+
+(defn remaining-cols [col dx]
+  "Returns the remaining columns into the given direction.
+   `dx<0` yields smaller columns, `dx>0` yields greater columns.
+   For `dx=0`, the current column is returned. Otherwise it is always excluded."
+  (cond
+    (= dx 0) [col]
+    (< dx 0) (subvec col-keys 0 (col-index col))
+    (> dx 0) (subvec col-keys (inc (col-index col)))))
+
+(defn remaining-rows [row dy]
+  "Returns the remaining rowumns into the given direction.
+   `dy<0` yields smaller rowumns, `dy>0` yields greater rowumns.
+   For `dy=0`, the current rowumn is returned. Otherwise it is always excluded."
+  (cond
+    (= dy 0) [row]
+    (< dy 0) (subvec row-keys 0 (row-index row))
+    (> dy 0) (subvec row-keys (inc (row-index row)))))
+
+(defn count-cols [col dx]
+  "Returns the number of cols remaining into the given direction.
+   `dx<0` yields smaller columns, `dx>0` yields greater columns."
+  (let [idx (col-index col)]
+    (if (neg? dx)
+      idx
+      (- 7 idx))))
+
+(defn count-rows [row dy]
+  "Returns the number of rows remaining into the given direction.
+   `dy<0` yields smaller columns, `dy>0` yields greater columns."
+  (let [idx (row-index row)]
+    (if (neg? dy)
+      idx
+      (- 7 idx))))
+
+(defn sgn [x]
+  "Returns the sign of an integer."
+  (case
+   (neg? x) -1
+   (pos? x) 1
+   :else 0))
+
+(defn line-of-sight
+  "Yields a set of all fields not hidden behind other pieces from a given board
+   position's point of view.
+   `dx` and `dy` hereby dictate the view direction.
+   For `dx<0` the check follows towards lower columns, for `dx>0` higher columns.
+   For `dy<0` the check follows towards lower rows, for `dy>0` higher rows."
+  [board [col row :as pos] [dx dy :as dir]]
+  (loop [[c r] pos
+         result #{}]
+    (let [new-pos (add-to-pos [c r] [dx dy])]
+      (cond
+        (nil? new-pos) result
+        (not= :empty (board-get board new-pos)) (conj result new-pos)
+        :else (recur new-pos (conj result new-pos))))))
--- test/chessjure/moves_test.clj
+++ test/chessjure/moves_test.clj
@@ -4,37 +4,100 @@
             [chessjure.moves :refer :all]))
 
 #_(deftest move-rook
-  (testing "only rook"
-    (let [board (board-put empty-board :white-rook :d :4)]
-      (is (valid-piece-move? board [:d :4] [:d :8])
-          "move up")
-      (is (valid-piece-move? board [:d :4] [:d :1])
-          "move down")
-      (is (valid-piece-move? board [:d :4] [:a :4])
-          "move left")
-      (is (valid-piece-move? board [:d :4] [:g :4])
-          "move up")
-      (is (not (valid-piece-move? board [:d :4] [:d :4]))
-          "do not keep still")
-      (is (not (valid-piece-move? board [:d :4] [:e :6]))
-          "do no move like knight")))
-  (testing "rook with same-colour pieces around"
-    (let [board (-> empty-board
-                    (board-put :white-rook :d :4)
-                    (board-put :white-pawn :f :4)
-                    (board-put :white-pawn :d :6)
-                    (board-put :white-pawn :b :4)
-                    (board-put :white-pawn :d :2))]
-      (is (not (valid-piece-move? board [:d :4] [:g :4]))
-          "don't pass right"))))
+    (testing "only rook"
+      (let [board (board-put empty-board :white-rook :d :4)]
+        (is (valid-piece-move? board [:d :4] [:d :8])
+            "move up")
+        (is (valid-piece-move? board [:d :4] [:d :1])
+            "move down")
+        (is (valid-piece-move? board [:d :4] [:a :4])
+            "move left")
+        (is (valid-piece-move? board [:d :4] [:g :4])
+            "move up")
+        (is (not (valid-piece-move? board [:d :4] [:d :4]))
+            "do not keep still")
+        (is (not (valid-piece-move? board [:d :4] [:e :6]))
+            "do no move like knight")))
+    (testing "rook with same-colour pieces around"
+      (let [board (-> empty-board
+                      (board-put :white-rook :d :4)
+                      (board-put :white-pawn :f :4)
+                      (board-put :white-pawn :d :6)
+                      (board-put :white-pawn :b :4)
+                      (board-put :white-pawn :d :2))]
+        (is (not (valid-piece-move? board [:d :4] [:g :4]))
+            "don't pass right"))))
 
 (deftest manipulate-position
   (testing "alter the field position"
     (is (= (add-to-pos [:a :1] [1 1]) [:b :2]))
     (is (= (add-to-pos [:a :1] [2 2]) [:c :3]))
     (is (= (add-to-pos [:c :3] [-2 -2]) [:a :1]))
-    (is (= (add-to-pos [:a :1] [-1 1]) [nil :2]))
-    (is (= (add-to-pos [:a :1] [1 -1]) [:b nil]))
+    (is (= (add-to-pos [:a :1] [-1 1]) nil))
+    (is (= (add-to-pos [:a :1] [1 -1]) nil))
     (is (= (add-to-pos [:a :1] [2 1]) [:c :2])
         "knights move")
-    (is (= (add-to-pos [:h :8] [1 1]) [nil nil]))))
+    (is (= (add-to-pos [:h :8] [1 1]) nil))))
+
+(deftest check-line-of-sight
+  (testing "directions"
+    (is (= #{[:a :2]} (line-of-sight empty-board [:b :2] [-1 0]))
+        "to the left")
+    (is (= #{[:b :1]} (line-of-sight empty-board [:b :2] [0 -1]))
+        "to the bottom")
+    (is (= #{[:h :7]} (line-of-sight empty-board [:g :7] [1 0]))
+        "to the right")
+    (is (= #{[:g :8]} (line-of-sight empty-board [:g :7] [0 1]))
+        "to the top")
+    (is (= #{[:a :1]} (line-of-sight empty-board [:b :2] [-1 -1]))
+        "to the bottom left")
+    (is (= #{[:a :8]} (line-of-sight empty-board [:b :7] [-1 1]))
+        "to the top left")
+    (is (= #{[:h :1]} (line-of-sight empty-board [:g :2] [1 -1]))
+        "to the bottom right")
+    (is (= #{[:h :8]} (line-of-sight empty-board [:g :7] [1 1]))
+        "to the top right"))
+  (testing "edges"
+    (is (= #{} (line-of-sight empty-board [:a :2] [-1 0]))
+        "at left edge going left")
+    (is (= #{} (line-of-sight empty-board [:h :2] [1 0]))
+        "at right edge going right")
+    (is (= #{} (line-of-sight empty-board [:a :8] [0 1]))
+        "at top edge going top")
+    (is (= #{} (line-of-sight empty-board [:a :1] [0 -1]))
+        "at bottom edge going bottom"))
+  (testing "paths to edges"
+    (is (= #{[:a :4] [:b :4] [:c :4]} (line-of-sight empty-board [:d :4] [-1 0]))
+        "to the left")
+    (is (= #{[:d :5] [:e :6] [:f :7] [:g :8]}
+           (line-of-sight empty-board [:c :4] [1 1]))
+        "to the top right")
+    (is (= #{}
+           (line-of-sight empty-board [:h :1] [1 -1]))
+        "to the bottom right from respective corner"))
+  (testing "path blocked by pieces"
+    (let [board (-> empty-board
+                    (board-put :white-pawn :b :4)
+                    (board-put :white-pawn :b :6)
+                    (board-put :white-pawn :d :6)
+                    (board-put :white-pawn :f :6)
+                    (board-put :white-pawn :f :4)
+                    (board-put :white-pawn :f :2)
+                    (board-put :white-pawn :d :2)
+                    (board-put :white-pawn :b :2))]
+      (is (= #{[:b :4] [:c :4]} (line-of-sight board [:d :4] [-1 0]))
+          "to the left")
+      (is (= #{[:b :6] [:c :5]} (line-of-sight board [:d :4] [-1 1]))
+          "to the top left")
+      (is (= #{[:d :5] [:d :6]} (line-of-sight board [:d :4] [0 1]))
+          "to the top")
+      (is (= #{[:e :5] [:f :6]} (line-of-sight board [:d :4] [1 1]))
+          "to the top right")
+      (is (= #{[:e :4] [:f :4]} (line-of-sight board [:d :4] [1 0]))
+          "to the right")
+      (is (= #{[:e :3] [:f :2]} (line-of-sight board [:d :4] [1 -1]))
+          "to the bottom right")
+      (is (= #{[:d :3] [:d :2]} (line-of-sight board [:d :4] [0 -1]))
+          "to the bottom")
+      (is (= #{[:b :2] [:c :3]} (line-of-sight board [:d :4] [-1 -1]))
+          "to the bottom left"))))
